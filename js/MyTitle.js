31a98cef9ffbb8b0c6513d37ff10036ec07ef539
“kasper341” <kasper_341@hotmail.com>
2016-12-28 10:12:20 -0800

    create seperate module for 'MyTilte' class

---
+++ js/MyTitle.js
@@ -0,0 +1,19 @@
+/* global React ReactDOM */
+
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var myTitle = React.createClass({
+
+  render () {
+    console.log(this.props)
+    return (
+      div(null,
+          // h1(null,    this.props.title)
+          h1({style: {color: this.props.color}}, this.props.title)//    passing colors
+      )
+    )
+  }
+})
+
+module.exports = myTitle
