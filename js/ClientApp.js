6adbcf8f353e4498efb1af20bc34c03d8e96ee4e
“kasper341” <kasper_341@hotmail.com>
2016-12-28 10:13:16 -0800

    refactor MyTile class

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,21 +1,7 @@
 /* global React ReactDOM */
 
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var myTitle = React.createClass({
-
-  render () {
-    console.log(this.props)
-    return (
-
-            div(null,
-                // h1(null,    this.props.title)
-                h1({style: {color: this.props.color}}, this.props.title)//    passing colors
-            )
-    )
-  }
-})
+var myTitle = require('./MyTitle')
 
 var myTitleFact = React.createFactory(myTitle)
 var ce = React.createElement
