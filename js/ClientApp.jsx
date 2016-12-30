cc7ce6969b1cc36d55067b4a19c118eb9ff828b0
“kasper341” <kasper_341@hotmail.com>
2016-12-29 22:17:40 -0800

    add formatting

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,20 +1,19 @@
 /* global React ReactDOM */
 var React = require('react')
 var ReactDOM = require('react-dom')
-var MyTitle = require('./MyTitle')
+// var MyTitle = require('./MyTitle')
 
 var MyFirstComponent = () => {
+  return (
 
-    return(
-
-      <div className='app-container'>
-       <div className='home-info'>
+    <div className='app-container'>
+      <div className='home-info'>
         <h1 className='title'></h1>
-        <input className='search' type="text" placeholder='Search'/>
+        <input className='search' type="text" placeholder='Search' />
         <button className='browse-all'>or Browse All</button>
-       </div>
       </div>
-    )
+    </div>
+  )
 }
 
 ReactDOM.render(<MyFirstComponent />, document.getElementById('app'))
