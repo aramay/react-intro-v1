e2df8629d1056e43b168e860d2844c30ba409bbd
“kasper341” <kasper_341@hotmail.com>
2016-12-30 15:54:49 -0800

    add validation for props

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,17 +1,30 @@
 const React = require('react')
 
 const ShowCard = (props) => {
+  { console.log(props) }
   return (
     <div className='show-card'>
-      <img src={`public/img/posters/${props.show.poster}`} className='show-card-img' />
+      <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
 
       <div className='show-card-text'>
-        <h3 className='show-card-title'>{props.show.title}</h3>
-        <h4 className='show-card-year'>({props.show.title})</h4>
-        <p className='show-card-description'>{props.show.description}</p>
+        <h3 className='show-card-title'>{props.title}</h3>
+        <h4 className='show-card-year'>({props.title})</h4>
+        <p className='show-card-description'>{props.description}</p>
       </div>
     </div>
   )
+}
+
+const { string1 } = React.PropTypes
+console.log(string1)
+
+const string = React.PropTypes.string
+console.log(string)
+
+ShowCard.propTypes = {
+  title: string.isRequired,
+  year: string.isRequired,
+  description: string.isRequired
 }
 
 module.exports = ShowCard
