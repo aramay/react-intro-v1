e2bcf4f59f6c79833c4707181456dbd35bc751c5
“kasper341” <kasper_341@hotmail.com>
2016-12-30 13:23:39 -0800

    add 'ShowCard' html

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -3,13 +3,15 @@ const React = require('react')
 const ShowCard = (props) => {
   return (
     <div className='show-card'>
-      <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
+      <img src={`public/img/posters/${props.show.poster}`} className='show-card-img' />
 
       <div className='show-card-text'>
-        <h3 className='show-card-title'>{show.title}</h3>
-        <h4 className='show-card-year'>({show.title})</h4>
-        <p className='show-card-description'>{show.description}</p>
+        <h3 className='show-card-title'>{props.show.title}</h3>
+        <h4 className='show-card-year'>({props.show.title})</h4>
+        <p className='show-card-description'>{props.show.description}</p>
       </div>
     </div>
   )
 }
+
+module.exports = ShowCard
