0ba616247efad0fd9f4d59542f82a01be1d0dbe7
“kasper341” <kasper_341@hotmail.com>
2016-12-30 10:54:35 -0800

    add html markup to show images

--- js/Search.jsx
+++ js/Search.jsx
@@ -4,18 +4,31 @@ const data = require('../public/data')
 const Search = () => {
   return (
     <div className='container'>
-      {data.shows.map((show) => {
-        return <h3>{show.title}</h3>
-      })}
+      <div className='shows'>
+        {data.shows.map((show) => {
+          {console.log(show)}
+          return (
+            <div className='show-card'>
+              <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
+
+              <div className='show-card-text'>
+                <h3 className='show-card-title'>{show.title}</h3>
+                <h4 className='show-card-year'>({show.title})</h4>
+                <p className='show-card-description'>{show.description}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
 
 module.exports = Search
 
-{/*
-  
+{ /*
+
 <pre><code>
       {JSON.stringify(shows, null, 4)}
     </code></pre>
-*/}
+*/ }
