e1d9f5a6ce228ace5a62b11f547b86460761f8e8
“kasper341” <kasper_341@hotmail.com>
2016-12-30 09:13:41 -0800

    Linl 'landing-page' to 'Search' page

---
+++ js/Landing.jsx
@@ -0,0 +1,17 @@
+const React = require('react')
+const { Link } = require('react-router')
+
+const Landing = () => {
+  return (
+
+    <div className='app-container'>
+      <div className='home-info'>
+        <h1 className='title'></h1>
+        <input className='search' type="text" placeholder='Search' />
+        <Link to='/search' className='browse-all'>or Browse All</Link>
+      </div>
+    </div>
+  )
+}
+
+module.exports = Landing
