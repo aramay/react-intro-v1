30f1ddbdde7acfb574e9ef4688fb310252676dea
“kasper341” <kasper_341@hotmail.com>
2016-12-30 16:15:20 -0800

    Add index route to landing page and Nest Routes to pass Layout as props

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -3,11 +3,12 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
+const Layout = require('./Layout')
 
 const ReactRouter = require('react-router')
 
 { /* destructuring es6 feature*/ }
-const { Router, Route, hashHistory } = ReactRouter
+const { Router, Route, IndexRoute, hashHistory } = ReactRouter
 
 { /* line 11-13, equivalent to line 8 */ }
 // const Router = ReactRouter.Router
@@ -17,8 +18,10 @@ const { Router, Route, hashHistory } = ReactRouter
 const App = () => {
   return (
     <Router history={hashHistory}>
-      <Route path='/' component={Landing} />
-      <Route path='/search' component={Search} />
+      <Route path='/' component={Layout}>
+        <IndexRoute component={Landing} />
+        <Route path='/search' component={Search} />
+      </Route>
     </Router>
   )
 }
