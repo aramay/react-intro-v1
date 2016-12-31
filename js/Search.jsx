01f5e4fd179a7811a3fd7bbdf5b68890f7b39fbe
“kasper341” <kasper_341@hotmail.com>
2016-12-30 20:08:08 -0800

    add 'state' to Search component add 'getInitialState()' method to get srach param state

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,20 +2,32 @@ const React = require('react')
 const data = require('../public/data')
 const ShowCard = require('./ShowCard')
 
-const Search = () => {
-  return (
-    <div className='container'>
-      <div className='shows'>
-        {data.shows.map((show) => {
-          return (
-            //spread operator (...)
-            <ShowCard {...show} key={show.imdbID} />
-          )
-        })}
+const Search = React.createClass({
+
+  getInitialState () {
+    return{
+      searchTerm: 'this is a search term'
+    }
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo{this.state.searchTerm}</h1>
+          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='search'/>
+        </header>
+        <div className='shows'>
+          {data.shows.map((show) => {
+            return (
+              //spread operator (...)
+              <ShowCard {...show} key={show.imdbID} />
+            )
+          })}
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
+})
 
 module.exports = Search
 
