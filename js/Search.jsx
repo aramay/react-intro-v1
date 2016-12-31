f9302ec7360456851bf0ee72674fa500d13c0db8
“kasper341” <kasper_341@hotmail.com>
2016-12-30 20:31:58 -0800

    add 'handleSearchTermEvent()' method to update search term

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,21 +5,24 @@ const ShowCard = require('./ShowCard')
 const Search = React.createClass({
 
   getInitialState () {
-    return{
+    return {
       searchTerm: 'this is a search term'
     }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
   },
   render () {
     return (
       <div className='container'>
         <header className='header'>
           <h1 className='brand'>svideo{this.state.searchTerm}</h1>
-          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='search'/>
+          <input value={this.state.searchTerm} type='text' className='search-input' placeholder='search' onChange={this.handleSearchTermEvent} />
         </header>
         <div className='shows'>
           {data.shows.map((show) => {
             return (
-              //spread operator (...)
+              // spread operator (...)
               <ShowCard {...show} key={show.imdbID} />
             )
           })}
