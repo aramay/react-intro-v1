c4a9d0c7e3a3c67d702d6b8bb72247fd4e9dd76e
“kasper341” <kasper_341@hotmail.com>
2016-12-30 16:16:08 -0800

    remove 'app-container' class

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -3,13 +3,10 @@ const { Link } = require('react-router')
 
 const Landing = () => {
   return (
-
-    <div className='app-container'>
-      <div className='home-info'>
-        <h1 className='title'></h1>
-        <input className='search' type="text" placeholder='Search' />
-        <Link to='/search' className='browse-all'>or Browse All</Link>
-      </div>
+    <div className='home-info'>
+      <h1 className='title'></h1>
+      <input className='search' type="text" placeholder='Search' />
+      <Link to='/search' className='browse-all'>or Browse All</Link>
     </div>
   )
 }
