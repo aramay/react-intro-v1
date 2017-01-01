3ba31ae8c3d5ff127834087974933194caa9f48c
“kasper341” <kasper_341@hotmail.com>
2017-01-01 13:28:23 -0800

    add redux-store connector to header

--- js/header.jsx
+++ js/header.jsx
@@ -1,16 +1,18 @@
 const React = require('react')
 const { Link } = require('react-router')
 const { func, bool, string } = React.PropTypes
+const { connector } = require('./Store')
 
 const Header = React.createClass({
 
   propTypes: {
     showSearch: bool,
     searchTerm: string,
-    handleSearchTermChange: func
+    handleSearchTermChange: func,
+    setSearchTerm: func
   },
   handleSearchTermEvent (event) {
-    this.props.handleSearchTermChange(event.target.value)
+    this.props.setSearchTerm(event.target.value)
   },
   render () {
     let utilSpace
@@ -40,4 +42,4 @@ const Header = React.createClass({
   }
 })
 
-module.exports = Header
+module.exports = connector(Header)
