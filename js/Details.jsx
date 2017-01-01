13bac673de3c547cdc7a6f36222582c5243911ae
“kasper341” <kasper_341@hotmail.com>
2016-12-31 21:52:08 -0800

    add html to display show 'Details'

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,15 +1,31 @@
 const React = require('react')
+const { object } = React.PropTypes
 
 const Details = React.createClass({
+  propTypes: {
+    params: object.isRequired
+  },
   render () {
-    return (
+    const params = this.props.params || {}
+    const { title, description, year, poster, trailer } = params
+    return (  
       <div className='container'>
-        <h1> details</h1>
-        <pre>
-          <code>
-            {JSON.stringify(this.props, null, 4)}
-          </code>
-        </pre>
+
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+        </header>
+
+        <div className=''video-info>
+          <h1 className='video-title'>{title}</h1>
+          <h2 className='video-year'>({year})</h2>
+          <img className='video-poster' src={`public/img/posters/${poster}`} alt={poster} />
+          <p className='description'>{description}</p>
+        </div>
+
+        <div className='video-container'>
+          <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen></iframe>
+        </div>
+
       </div>
 
     )
