26d9d5fa78c221036373b9703b1c3063cc072e7b
“kasper341” <kasper_341@hotmail.com>
2017-01-01 13:42:40 -0800

    refactor 'Header' component to use Store

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,35 +2,38 @@ const React = require('react')
 const ShowCard = require('./ShowCard')
 const Header = require('./Header')
 
-const { object } = React.PropTypes
+const { object, string } = React.PropTypes
+
+const { connector } = require('./Store')
 
 const Search = React.createClass({
 
-  getInitialState () {
-    return {
-      searchTerm: ''
-    }
-  },
+  // getInitialState () {
+  //   return {
+  //     searchTerm: ''
+  //   }
+  // },
   // propTypes can live inside component when using createClass
   propTypes: {
-    route: object
+    route: object,
+    searchTerm: string
   },
-  handleSearchTermChange (searchTerm) {
-    this.setState({searchTerm: searchTerm})
-  },
+  // handleSearchTermChange (searchTerm) {
+  //   this.setState({searchTerm: searchTerm})
+  // },
   render () {
     return (
       <div className='container'>
 
         <Header
-          handleSearchTermChange={this.handleSearchTermChange}
-          searchTerm={this.state.searchTerm}
+          // handleSearchTermChange={this.handleSearchTermChange}
+          // searchTerm={this.state.searchTerm}
           showSearch
         />
 
         <div className='shows'>
           {this.props.route.shows.filter((show) => {
-            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            return `${show.title} ${show.description}`.toUpperCase().indexOf(this.props.searchTerm.toUpperCase()) >= 0
           })
           .map((show) => {
             return (
@@ -45,4 +48,4 @@ const Search = React.createClass({
   }
 })
 
-module.exports = Search
+module.exports = connector(Search)
