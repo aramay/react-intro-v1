14ac49057d1d4b7a6d297b14d17609c63ff4d448
“kasper341” <kasper_341@hotmail.com>
2017-01-01 11:41:27 -0800

    refactor Header component to take props

--- js/header.jsx
+++ js/header.jsx
@@ -1,9 +1,41 @@
 const React = require('react')
+const { Link } = require('react-router')
+const { func, bool, string } = React.PropTypes
 
 const Header = React.createClass({
+
+  propTypes: {
+    showSearch: bool,
+    searchTerm: string,
+    handleSearchTermChange: func
+  },
+  handleSearchTermEvent (event) {
+    this.props.handleSearchTermChange(event.target.value)
+  },
   render () {
+    let utilSpace
+
+    if (this.props.showSearch) {
+      utilSpace = <input type='text'
+        className='search-input'
+        placeholder='search'
+        value={this.props.searchTerm}
+        onChange={this.handleSearchTermEvent}
+      /> // end of input
+    } else {
+      utilSpace = (
+        <h2 className='header-back'>
+          <Link to='/search'>Back</Link>
+        </h2>
+      )
+    }
     return (
-      <h1>header</h1>
+      <header className='header'>
+        <h1 className='brand'>
+          <Link to='/' className='brand-link'>svideo</Link>
+        </h1>
+        {utilSpace}
+      </header>
     )
   }
 })
