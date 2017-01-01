38910d73aeea53a3a714ebd7ca966306271d2f1a
“kasper341” <kasper_341@hotmail.com>
2017-01-01 12:06:05 -0800

    add 'Store' redux

---
+++ js/Store.jsx
@@ -0,0 +1,12 @@
+const redux = require('redux')
+const reactRedux = require('react-redux')
+
+const SET_SEARCH_TERM = 'setSearchTerm'
+
+const reducer = (state, action) => {
+  switch(action.type){
+    
+  }
+}
+
+
