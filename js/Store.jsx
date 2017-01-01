8c89aaca2b26f370e96eaea369fa6fe0db08970e
“kasper341” <kasper_341@hotmail.com>
2017-01-01 12:21:39 -0800

    add 'Reducer' in redux store

--- js/Store.jsx
+++ js/Store.jsx
@@ -4,9 +4,16 @@ const reactRedux = require('react-redux')
 const SET_SEARCH_TERM = 'setSearchTerm'
 
 const reducer = (state, action) => {
-  switch(action.type){
-    
+  switch(action.type) {
+    case SET_SEARCH_TERM:
+      return reduceSearch(state, action)
+    default:
+      return state
   }
 }
 
-
+const reduceSearch = (state, action) => {
+  const newState = {}
+  Object.assign(newState, state, {searchTerm: action.value})
+  return newState
+}
