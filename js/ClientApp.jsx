6a940aafea9aa73e681b471469458c7e6f5d54c0
“kasper341” <kasper_341@hotmail.com>
2016-12-31 20:44:26 -0800

    add route for 'details' component

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,6 +4,9 @@ const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const Search = require('./Search')
 const Layout = require('./Layout')
+const Details = require('./Details')
+
+const { shows } = require('../public/data')
 
 const ReactRouter = require('react-router')
 
@@ -20,7 +23,8 @@ const App = () => {
     <Router history={hashHistory}>
       <Route path='/' component={Layout}>
         <IndexRoute component={Landing} />
-        <Route path='/search' component={Search} />
+        <Route path='/search' component={Search} shows={shows} />
+        <Route path='/details/:id' component={Details} />
       </Route>
     </Router>
   )
