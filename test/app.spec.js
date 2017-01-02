934c858c8e25fc96fcd9e0d3077a743ddf7b4dff
“kasper341” <kasper_341@hotmail.com>
2017-01-02 14:24:30 -0800

    add test for Redux Store

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,12 +3,13 @@ const { expect } = require('chai')
 const React = require('react')
 const Search = require('../js/Search')
 const ShowCard = require('../js/ShowCard')
+const { store, rootReducer } = require('../js/Store.jsx')
 
 const { shallow, mount } = require('enzyme')
 const { shows } = require('../public/data')
 
 
-describe('<Search />', () => {
+xdescribe('<Search />', () => {
   it('It should render brand', () => {
     const wrapper = shallow(<Search/>)
     expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
@@ -26,6 +27,20 @@ describe('<Search />', () => {
     expect(wrapper.state('searchTerm')).to.equal('house')
     expect(wrapper.find('.show-card').length).to.equal(2)
 
+  })
+
+})
+
+describe('Store', () => {
+  it('Should bootstrap', () => {
+    // goes from having nothing
+    const state = rootReducer(undefined, { type: '@@redux/INIT'})
+    // creat this initial state to test
+    expect(state).to.deep.equal({searchTerm: ''})
+  })
+  it('Should handle setSearchTerm action', () => {
+    const state = rootReducer({searchTerm: 'some random string'}, {type: 'setSearchTerm', value: 'correct string'})
+    expect(state).to.deep.equal({searchTerm: 'correct string'})
   })
 })
 
