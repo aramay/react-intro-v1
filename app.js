f51c78914f5905cef2709af1a62c23c376a85c00
“kasper341” <kasper_341@hotmail.com>
2017-01-04 12:41:51 -0800

    add 'universal-rendering' code

--- app.js
+++ app.js
@@ -12,8 +12,37 @@ const Store = require('./js/Store.jsx')
 const store = Store.store
 const _ = require('lodash')
 const fs = require('fs')
-const port = 5050
+const port = 8000
 const baseTemplate = fs.readFileSync('./index.html')
 const template = _.template(baseTemplate)
 const ClientApp = require('./js/ClientApp.jsx')
 const routes = ClientApp.Routes
+
+console.log(routes())
+console.log(ReactRedux)
+
+const app = express()
+
+app.use('/public', express.static('./public'))
+
+app.use((req, res) => {
+  match({ routes: routes(), location: req.url }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      res.status(500).send(error.message)
+    } else if (redirectLocation) {
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      const body = ReactDOMServer.renderToString(
+        React.createElement(Provider, {store: store},
+          React.createElement(RouterContext, renderProps)
+        )
+      )
+      res.status(200).send(template({body: body}))
+    } else {
+      res.status(404).send('Not found')
+    }
+  })
+})
+
+console.log('listening on ' + port)
+app.listen(port)
