86353e83bc453e818fbb92cbb136e45e74031c16
“kasper341” <kasper_341@hotmail.com>
2017-01-05 11:08:13 -0800

    setup Ajax call to IMDB DB

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,6 +2,7 @@ const React = require('react')
 const Header = require('./Header')
 const { arrayOf, object, string } = React.PropTypes
 const { connector } = require('./Store')
+const { axios } = require('axios')
 
 const Details = React.createClass({
   propTypes: {
@@ -15,10 +16,33 @@ const Details = React.createClass({
     })
     return showArray[0]
   },
+
+  getInitialState () {
+    return {
+      omdbData: {}
+    }
+  },
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.assignShow(this.props.params.id).imdbID}`)
+    .then((response) => {
+      this.setState({omdbData: response.data})
+    })
+    .catch((error) => {
+      console.log('axios error ', error)
+    })
+  },
   render () {
     // const params = this.props.params || {}
     // const { title, description, year, poster, trailer } = params
     const { title, description, year, poster, trailer } = this.assignShow(this.props.params.id)
+
+    let rating
+
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3 className='video-rating'>
+        {this.state.omdbData.imdbRating}
+      </h3>
+    }
     return (
       <div className='container'>
 
@@ -27,7 +51,10 @@ const Details = React.createClass({
         <div className=''video-info>
           <h1 className='video-title'>{title}</h1>
           <h2 className='video-year'>({year})</h2>
-          <img className='video-poster' src={`public/img/posters/${poster}`} alt={poster} />
+
+          {rating}
+
+          <img className='video-poster' src={`/public/img/posters/${poster}`} alt={poster} />
           <p className='description'>{description}</p>
         </div>
 
